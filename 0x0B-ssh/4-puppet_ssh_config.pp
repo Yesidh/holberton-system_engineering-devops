4edba8d956470278f341f8eccfa1e9611976265b
Yesidh <944@holbertonschool.com>
2019-12-10 18:06:29 -0500

    [Feature]: Changing ssh_config using puppet manifiest

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+# conection to server using ssh:
+#    our SSH client configuration must be configured to use the private key ~/.ssh/holberton
+#    Your SSH client configuration must be configured to refuse to authenticate using a password
+
+exec {'~/.ssh/holberton':
+      command => '/bin/echo --full PasswordAuthentication no \nIdentityFile ~/.ssh/holberton >> /etc/ssh/ssh_config'
+      }

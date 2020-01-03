540677110560b6d4a3696ed6e9aa5b089a8b99a9
Yesidh <944@holbertonschool.com>
2020-01-03 00:43:26 -0500

    [Fix]: adding -> at the ent of the first commands

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -3,12 +3,12 @@
 exec { 'install nginx':
   command => 'sudo apt-get -y update && sudo apt-get -y install nginx',
   provider => shell,
-}
+} ->
 
 exec { 'http header':
   command => 'sudo sed -i "21i add_header X-Served-By \$hostname;" /etc/nginx/nginx.conf',
   provider => shell,
-}
+} ->
 
 exec { 'restar nginx':
   command => 'sudo service nginx restart',

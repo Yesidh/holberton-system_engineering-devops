8e86c9418cb4135f43dbe90feca73bcb006aff47
Yesidh <944@holbertonschool.com>
2020-01-03 00:38:00 -0500

    [Fix]: adding , at the end in the sed command

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -3,10 +3,10 @@
 exec { 'install nginx':
   command => 'sudo apt-get -y update && sudo apt-get -y install nginx',
   provider => shell,
-     }
+}
 
 exec { 'http header':
-  command => 'sudo sed -i "21i add_header X-Served-By \$hostname;" /etc/nginx/nginx.conf'
+  command => 'sudo sed -i "21i add_header X-Served-By \$hostname;" /etc/nginx/nginx.conf',
   provider => shell,
 }
 

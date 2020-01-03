ffcbc0d138fec905312cf6d14edb7e380a5fc070
Yesidh <944@holbertonschool.com>
2020-01-03 00:24:17 -0500

    [Fix]: adding puppet code to install nginx and configure http header

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,7 +1,16 @@
-#!usr/bin/env bash
 # bash script that install nginx and configure http header
 
+exec {'install nginx':
+  command => 'sudo apt-get -y update && sudo apt-get -y install nginx',
+  provider => 'shell',
+     }
+
 exec {'http header':
-     command => "sudo apt-get -y update && sudo apt-get -y install nginx && sudo sed -i "21i add_header X-Served-By \$hostname;" /etc/nginx/nginx.conf && sudo service nginx restart",
-     provider => shell,
+  command => 'sudo sed -i "21i add_header X-Served-By \$hostname;" /etc/nginx/nginx.conf'
+  provider => shell,
+}
+
+exec {'restar nginx':
+  command => 'sudo service nginx restart',
+  provider => shell,
 }

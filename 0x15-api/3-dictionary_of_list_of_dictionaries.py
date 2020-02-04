d10c8cadc4b8ada462ef81c49e91085d8a031813
Yesidh <944@holbertonschool.com>
2020-02-04 00:07:59 -0500

    [perrada time]

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+=========================================================================
+Export to CSV:
+Python script that, using this https://jsonplaceholder.typicode.com/, for
+a given employee ID, returns information about his/her TODO list progress.
+Requirements:
+    Records all tasks that are owned by this employee
+    Format must be: { "USER_ID": [ {"task": "TASK_TITLE", "completed":
+         TASK_COMPLETED_STATUS, "username": "USERNAME"}}, {"task":
+         "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username":
+         "USERNAME"}}, ... ]}
+    File name must be: USER_ID.json
+=========================================================================
+"""
+
+import json
+import requests
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    url2 = 'https://jsonplaceholder.typicode.com/todos/'
+    r = requests.get(url).json()
+    r2 = requests.get(url2).json()
+    my_list = []
+    my_dict = {}
+    with open('todo_all_employees.json', 'w') as file:
+        for item in range(0, len(r)):
+            for user in r2:
+                user['task'] = user.pop('title')
+                user.pop('id')
+                user.pop('userId')
+                user['username'] = r
+                my_list.append(user)
+            my_dict['{}'.format(r[item].get('id'))] = my_list
+        json.dump(my_dict, file)

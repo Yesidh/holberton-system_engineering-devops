07298a0ec734678f66f2d8e4f5038674ba5b7801
Yesidh <944@holbertonschool.com>
2020-02-06 16:29:35 -0500

    [Fix]: adding limit in the url request

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -14,12 +14,11 @@ def top_ten(subreddit):
     function that queries the Reddit API and prints the titles of the first
     10 hot posts listed for a given subreddit.
     """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     header = {'User-Agent': 'custom agent'}
     r = requests.get(url, headers=header, allow_redirects=False)
     if r.status_code == 200:
-        for i in range(10):
-            print(r.json().get('data').get('children')[i].get('data').
-                  get('title'))
+        for i in r.json().get('data').get('children'):
+            print(i.get('data').get('title'))
     else:
         return None

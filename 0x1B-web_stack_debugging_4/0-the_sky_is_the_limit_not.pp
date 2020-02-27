15cecc4a81748e139da66e06b71e5210ed188988
Yesidh <944@holbertonschool.com>
2020-02-27 13:13:31 -0500

    [Fix]: adding sudo

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,7 @@
 # change values in the file /etc/default/nginx/
 
 exec {'concurrence_conections'
-  command => "/usr/bin/sed -i 's/-n 15/-n 1000' /etc/default/nginx\
-  service nginx restart"
+  command => "/usr/bin/sed -i 's/-n 15/-n 1000' /etc/default/nginx",
+  command => 'sudo service nginx restart',
+  provider => shell
   }

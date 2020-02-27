bcbdb11ea5e3f26c23f7feaa57ed308304903824
Yesidh <944@holbertonschool.com>
2020-02-27 13:36:04 -0500

    [Fix]:ya no se que hacer

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # change values in the file /etc/default/nginx/
 
 exec {'concurrence_conections'
-  command => "sudo sed -i \'s/15/1000\' /etc/default/nginx && sudo service nginx restart",
+  command  => "sudo sed -i \'s/15/1000/\' /etc/default/nginx && sudo service nginx restart",
   provider => shell,
 }

2ccefd884e073fd46544e1b1fb093fa1c4b1b24e
Yesidh <944@holbertonschool.com>
2020-02-27 13:19:05 -0500

    [Fix]: changing 5 by 15

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # change values in the file /etc/default/nginx/
 
 exec {'concurrence_conections'
-  command => "sudo sed -i 's/5/1000' /etc/default/nginx",
+  command => "sudo sed -i 's/15/1000' /etc/default/nginx",
   command => 'sudo service nginx restart',
   provider => shell
   }

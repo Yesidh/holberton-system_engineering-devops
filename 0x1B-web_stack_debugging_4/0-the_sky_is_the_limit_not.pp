174b0a29fc85777e636fa5c22a5c93f8a05204c4
Yesidh <944@holbertonschool.com>
2020-02-27 13:21:20 -0500

    [fix]: changing sudo by /usr/bin/ in sed command

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # change values in the file /etc/default/nginx/
 
 exec {'concurrence_conections'
-  command => "sudo sed -i 's/15/1000' /etc/default/nginx",
+  command => "/usr/bin/sed -i 's/15/1000' /etc/default/nginx",
   command => 'sudo service nginx restart',
-  provider => shell
-  }
+  provider => shell,
+}

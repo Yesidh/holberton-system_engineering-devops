11d7af7d9214900c8af14c6b9452e43c43b4499b
Yesidh <944@holbertonschool.com>
2020-02-27 13:05:20 -0500

    [Fix]: changing the structure command

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,6 @@
 # changing values hard and soft in the file limits.conf
 
-exex {'soft_hard_feature':
-  command => "sed -i 's/holberton hard nofile 5/holberton hard nofile 50' /etc/security/limits.conf";
-  command => "sed -i 's/holberton soft nofile 4/holberton soft nofile 40' /etc/security/limint.conf";
-  path    => '/usr/bin'
+exec {'soft_hard_feature':
+  command => "/usr/bin/sed -i 's/holberton hard nofile 5/holberton hard nofile 50' /etc/security/limits.conf;\
+  /usr/bin/sed -i 's/holberton soft nofile 4/holberton soft nofile 40' /etc/security/limits.conf"
 }

318d886a1dfa55e372281840e970d14db992c072
Yesidh <944@holbertonschool.com>
2020-02-27 13:16:11 -0500

    [Fix]: deleting the flag -n in sed command

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # change values in the file /etc/default/nginx/
 
 exec {'concurrence_conections'
-  command => "/usr/bin/sed -i 's/-n 15/-n 1000' /etc/default/nginx",
+  command => "sudo sed -i 's/5/1000' /etc/default/nginx",
   command => 'sudo service nginx restart',
   provider => shell
   }

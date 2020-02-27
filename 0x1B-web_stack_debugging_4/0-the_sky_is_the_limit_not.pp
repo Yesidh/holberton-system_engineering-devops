c432d22dd8adf87d4023f988648ca29e4fc9e38e
Yesidh <944@holbertonschool.com>
2020-02-27 13:27:28 -0500

    [fix]: omitiendo comillas

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # change values in the file /etc/default/nginx/
 
 exec {'concurrence_conections'
-  command => "/usr/bin/sed -i 's/15/30000' /etc/default/nginx",
+  command => "/usr/bin/sed -i \'s/15/30000\' /etc/default/nginx",
   command => 'sudo service nginx restart',
   provider => shell,
 }

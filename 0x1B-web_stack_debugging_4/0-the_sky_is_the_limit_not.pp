9067ecd7f1da67ba0f930c12062380f62c4a08e0
Yesidh <944@holbertonschool.com>
2020-02-27 13:41:12 -0500

    [Fix]: puppet-lint style

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,6 @@
 # change values in the file /etc/default/nginx/
 
 exec {'concurrence_conections':
-  command  => "/usr/bin/sed -i 's/15/1000/' /etc/default/nginx",
-  command  => 'sudo service nginx restart',
+  command  => "/usr/bin/sed -i 's/15/1000/' /etc/default/nginx && sudo service nginx restart",
   provider => shell,
 }

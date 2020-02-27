97a23b0182813eb3496414146fc14f0ac9e37bca
Yesidh <944@holbertonschool.com>
2020-02-27 12:59:25 -0500

    [Fix]: adding a path argument in the puppet manifest

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,7 @@
 # changing values hard and soft in the file limits.conf
 
 exex {'soft_hard_feature':
-  command => "/usr/bin/sed -i 's/holberton hard nofile 5/holberton hard nofile 50' /etc/security/limits.conf";
-  command => "/usr/bin/sed -i 's/holberton soft nofile 4/holberton soft nofile 40' /etc/security/limint.conf";
+  command => "sed -i 's/holberton hard nofile 5/holberton hard nofile 50' /etc/security/limits.conf";
+  command => "sed -i 's/holberton soft nofile 4/holberton soft nofile 40' /etc/security/limint.conf";
+  path    => '/usr/bin'
 }
